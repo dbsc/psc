2c2372d6bf107a28c65d3eb36fad84c7b48966bf
André Dal Bosco <andredbsc@gmail.com>
2023-09-19 18:41:19 +0000

    fixed lean build process

--- Makefile
+++ Makefile
@@ -1,9 +1,15 @@
-.PHONY: charon aeneas
+.PHONY: charon aeneas clean
 
 all: charon aeneas
+
+lake:
+	lake build
 
 charon:
 	charon --opaque=main
 
 aeneas:
 	aeneas.exe psc.llbc -backend lean
+
+clean:
+	rm -rf *.llbc
--- lakefile.lean
+++ lakefile.lean
@@ -1,12 +1,18 @@
 import Lake
 open Lake DSL
 
+require base from "/aeneas/backends/lean"
+
 package «psc» {
   -- add package configuration options here
 }
 
 lean_lib «Psc» {
   -- add library configuration options here
+}
+
+lean_lib «Base» {
+  srcDir := "/aeneas/backends/lean"
 }
 
 @[default_target]
